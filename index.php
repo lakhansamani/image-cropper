9516cff24970efb3cccf53515035b71820aca883
lakhansamani <lakhan.m.samani@gmail.com>
2015-03-18 01:27:15 +0530

    Adds index.php with basic YUI assests

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Image Cropper</title>
+		<!-- Stylesheet required for image from yui library -->
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/resize/assets/skins/sam/resize.css" />
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/imagecropper/assets/skins/sam/imagecropper.css" />
+		<!-- JS required for image from yui library -->
+		<script type="text/javascript" src="http://yui.yahooapis.com/combo?2.5.2/build/yahoo-dom-event/yahoo-dom-event.js&;2.5.2/build/dragdrop/dragdrop-min.js&2.5.2/build/element/element-beta-min.js&2.5.2/build/resize/resize-beta-min.js&2.5.2/build/imagecropper/imagecropper-beta-min.js&2.5.2/build/connection/connection-min.js&2.5.2/build/json/json-min.js">
+		</script>
+	</head>
+	<body>
+		
+	</body>
+</html>
+
+
